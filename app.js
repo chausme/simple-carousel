1a2fb5fc0c9dbe8629cafd201d6ca90ac20c18da
Alex Chaus <chausme@gmail.com>
2022-11-08 06:11:59 +0700

    Add buttons and initial data

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('simple carousel');
+const reviews = [
+    {
+        id: 1,
+        name: 'Susan Smith',
+        job: 'Web Developer',
+        img: './images/person-1.jpg',
+        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus hic aut maiores repellendus omnis praesentium officiis! Autem deleniti tenetur eum.',
+    },
+];
